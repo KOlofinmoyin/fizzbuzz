fc4ff169046aede3c37c601dc135a622af725d1e
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2019-11-12 15:28:27 +0000

    red->green tests for isDivisibleByFive

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -7,10 +7,21 @@ describe('Fizzbuzz', function() {
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
 
+    it('divisible by 5', function(){
+      fizzbuzz = new Fizzbuzz();
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
+  });
   describe('knows when a number is NOT', function(){
     it('divisible by 3', function(){
-        fizzbuzz = new Fizzbuzz();
-        expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+      fizzbuzz = new Fizzbuzz();
+      expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+    });
+
+    it('divisible by 5', function(){
+      fizzbuzz = new Fizzbuzz();
+      expect(fizzbuzz.isDivisibleByFive(6)).toBe(false);
     });
   });
 

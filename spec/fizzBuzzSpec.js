4f83d58c8da02ca43c640c95db61f1f6ec9a23f2
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2019-11-12 15:47:47 +0000

    refactored with adding beforeEach to add fizzbuzz = new Fizzbuzz(); at start of tests.

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,36 +1,33 @@
 describe('Fizzbuzz', function() {
   var fizzbuzz;
+  beforeEach(function() {
+    fizzbuzz = new Fizzbuzz();
+  });
 
   describe('knows when a number is', function(){
     it('divisible by 3', function(){
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
 
     it('divisible by 5', function(){
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
     });
 
     it('is divisible by 3 and 5', function(){
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByThreeAndFive(15)).toBe(true);
     });
 
   });
   describe('knows when a number is NOT', function(){
     it('divisible by 3', function(){
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
     });
 
     it('divisible by 5', function(){
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByFive(6)).toBe(false);
     });
 
     it('is not divisible by 3 or 5', function(){
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByThreeAndFive(17)).toBe(false);
     });
   });
